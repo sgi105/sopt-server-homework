cdf275057c8e058340b4fbce79c492d05b91365f
Ga In Shin <sgi105@github.com>
2019-05-10 23:33:47 +0900

    a

--- 3차 실습/routes/homework/board.js
+++ 3차 실습/routes/homework/board.js
@@ -11,7 +11,8 @@ const {
   SAVE_FILE_FAIL,
   CREATED_USER,
   CREATED_POST,
-  SAME_TITLE_EXISTS
+  SAME_TITLE_EXISTS,
+  DELETE_POST
 } = require("../../module/utils/responseMessage");
 const {
   OK,
@@ -23,38 +24,101 @@ const absolutePath = __dirname + "/data.csv";
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
+  console.log(id);
+  getPostById(absolutePath, id).then(post => res.status(OK).send(post));
 });
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   const post = req.body;
 
   post.time = moment().format("MMMM Do YYYY, h:mm:ss a");
-  checkForSameTitle(absolutePath, post);
-  hashPasswordAndSave(post, res);
+  checkForSameTitle(absolutePath, post, res);
+  hashPasswordAndAppend(post, res);
 });
 
-router.put("/", (req, res) => {});
+router.put("/:id", (req, res) => {
+  getPostByIdAndUpdate(absolutePath, req.params.id, res);
+});
 
-router.delete("/", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  getPostByIdAndDelete(absolutePath, id).then(posts =>
+    AppendToDbAndSendResponse(absolutePath, posts, res, DELETE_POST)
+  );
+});
 
 console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
-module.exports = router;
 
-const checkForSameTitle = function(filePath, newPost) {
+function checkForSameTitle(filePath, newPost, res) {
+  csv()
+    .fromFile(filePath)
+    .then(posts => {
+      posts.forEach(post => {
+        if (post.title == newPost.title)
+          return res.status(BAD_REQUEST).send(SAME_TITLE_EXISTS);
+      });
+    })
+    .catch(err => console.log(err));
+}
+
+function getPostById(filePath, id) {
   return new Promise((resolve, reject) => {
     csv()
       .fromFile(filePath)
       .then(posts => {
-        posts.forEach(post => {
-          if (post.title === newPost.title)
-            req.status(BAD_REQUEST).send(SAME_TITLE_EXISTS);
+        console.log(posts);
+
+        posts.forEach((post, index) => {
+          if (post.id == id) resolve(post);
         });
       })
       .catch(err => console.log(err));
   });
-};
+}
 
-function hashPasswordAndSave(post, res) {
+function getPostByIdAndDelete(filePath, id) {
+  return new Promise((resolve, reject) => {
+    csv()
+      .fromFile(filePath)
+      .then(posts => {
+        console.log(posts);
+        let indexToDelete = undefined;
+        posts.forEach((post, index) => {
+          if (post.id == id) {
+            indexToDelete = index;
+          }
+          posts.splice(indexToDelete, 1);
+          console.log(posts);
+          resolve(posts);
+        });
+      })
+      .catch(err => console.log(err));
+  });
+}
+
+function getPostByIdAndUpdate(filePath, id, res) {
+  return new Promise((resolve, reject) => {
+    csv()
+      .fromFile(filePath)
+      .then(posts => {
+        console.log(posts);
+        let indexToDelete = undefined;
+        posts.forEach((post, index) => {
+          if (post.id == id) {
+            post = req.body;
+            post.time = moment().format("MMMM Do YYYY, h:mm:ss a");
+            checkForSameTitle(absolutePath, post, res);
+            hashPasswordAndWrite(post, res);
+          }
+          resolve(posts);
+        });
+      })
+      .catch(err => console.log(err));
+  });
+}
+
+function hashPasswordAndAppend(post, res) {
   crypto.randomBytes(32, (err, buffer) => {
     if (err) {
       res.status(DB_ERROR).send(err);
@@ -73,7 +137,7 @@ function hashPasswordAndSave(post, res) {
             // 정상적으로 hashing이 완료되면 message, hashedPassword, salt를 userData에 저장
             post.hashedPassword = hashed.toString("base64");
             post.salt = buffer.toString("base64");
-            writeToDbAndSendRespose(absolutePath, post, res, CREATED_POST);
+            AppendToDbAndSendResponse(absolutePath, post, res, CREATED_POST);
           }
         }
       );
@@ -81,7 +145,34 @@ function hashPasswordAndSave(post, res) {
   });
 }
 
-function writeToDbAndSendRespose(filePath, data, res, message) {
+function hashPasswordAndWrite(post, res) {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      res.status(DB_ERROR).send(err);
+    } else {
+      //   생성된 randomByte를 salt로 사용하여 password hashing하기
+      crypto.pbkdf2(
+        post.password,
+        buffer.toString("base64"),
+        10000,
+        64,
+        "sha512",
+        (err, hashed) => {
+          if (err) {
+            res.status(DB_ERROR).send(err);
+          } else {
+            // 정상적으로 hashing이 완료되면 message, hashedPassword, salt를 userData에 저장
+            post.hashedPassword = hashed.toString("base64");
+            post.salt = buffer.toString("base64");
+            WriteToDbAndSendResponse(absolutePath, post, res, CREATED_POST);
+          }
+        }
+      );
+    }
+  });
+}
+
+function AppendToDbAndSendResponse(filePath, data, res, message) {
   const resultCSV = json2csv.parse(data, { header: false });
   fs.appendFile(filePath, "\n" + resultCSV, err => {
     if (err) {
@@ -91,3 +182,16 @@ function writeToDbAndSendRespose(filePath, data, res, message) {
     }
   });
 }
+
+function WriteToDbAndSendResponse(filePath, data, res, message) {
+  const resultCSV = json2csv.parse(data, { header: false });
+  fs.writeFile(filePath, "\n" + resultCSV, err => {
+    if (err) {
+      res.status(DB_ERROR).send(SAVE_FILE_FAIL);
+    } else {
+      res.status(OK).send(message);
+    }
+  });
+}
+
+module.exports = router;
